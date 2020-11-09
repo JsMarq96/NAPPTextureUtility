d4ed2441b8e6a8aac30515fc86cb8c3289876fd1
JsMarq96 <jsmarquerie@gmail.com>
2020-11-09 17:45:15 +0100

    Fixed utility and prepare it for GUI

--- Napp_Gen.py
+++ Napp_Gen.py
@@ -5,18 +5,26 @@ def resize(origin_folder, result_folder, scale):
     resize_directory(origin_folder, result_folder, scale)
 
 
-def compress(origin_folder, result_folder):
+def compress(origin_folder, result_folder, comp_dict):
     result_dir = directory_clone(origin_folder, result_folder, '_Zero')
-    compress_directory(result_dir,  { 'COLOR': CMODE.LIGHT,
-                                       'NORN': CMODE.LIGHT,
-                                       'SPEC': CMODE.HEAVY
-                                     })
+    compress_directory(result_dir, comp_dict)
 
+COMP_DICT = { 'Light': { 'COLOR': CMODE.LIGHT,
+                        'NORN': CMODE.LIGHT,
+                         'SPEC': CMODE.LIGHT},
+               'Hard': { 'COLOR': CMODE.HEAVY,
+                        'NORN': CMODE.HEAVY,
+                        'SPEC': CMODE.HEAVY},
+               'Light w/Hard Speculars': { 'COLOR': CMODE.LIGHT,
+                                           'NORN': CMODE.LIGHT,
+                                           'SPEC': CMODE.HEAVY}
+             }
 
-def generate_resource_packs(base_resource_pack, variations, result_folder):
-    for var in variations:
-        if var.scale:
-            resize(base_resource_pack, result_folder, var.scale)
+def generate_resourcepacks(base_resource_pack, variations, result_folder):
+    for pack in variations:
+        spl_p = pack.split('-')
 
-        if var.compress:
-            compress(base_resource_pack, result_folder)
+        resize(base_resource_pack, int(spl_p[0]), result_folder )
+
+        if spl_p[1] != 'None':
+            compress(base_resource_pack, result_folder, COMP_DICT[spl_p[1]])
--- TexPackResize.py
+++ TexPackResize.py
@@ -24,7 +24,8 @@ def image_scale(img_adress, result_img_adress, new_size):
         Function to clone a full folder directory, in order to scale it
     '''
 def directory_clone(directory, location='', add_on=''):
-    new_dir_name = os.path.join(location, os.path.basename(directory) + '_' + add_on)
+    print(directory, 'add on: ', add_on)
+    new_dir_name = os.path.join(location, os.path.basename(directory+ '_' + str(add_on)))
     print(new_dir_name)
     shutil.copytree(directory, new_dir_name)
         
@@ -46,8 +47,9 @@ def scale_directory(directory, img_types, scale):
      Duplicates a directory and then scales it
 '''
 def resize_directory(directory, scale, address='', img_types = IMAGE_TYPES):
-    new_dir = directory_clone(directory, address, str(scale[0]) + 'x')
-    scale_directory(os.path.join(address,new_dir), img_types, scale)
+    print(scale)
+    new_dir = directory_clone(directory, address, str(scale) + 'x')
+    scale_directory(os.path.join(address,new_dir), img_types, (scale, scale))
 
 
 '''
