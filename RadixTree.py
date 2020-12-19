8a5769a602bd82f803db53a5da6e6e4d59a0f0b6
JsMarq96 <jsmarquerie@gmail.com>
2020-12-19 13:08:42 +0100

    halt the radix tree functionality, for now

--- RadixTree.py
+++ RadixTree.py
@@ -37,13 +37,43 @@ class RadixTree:
         # Case 2: Kinda similar on the end
         # Case 3: Similar on the start, up to the middle
         # Are 2 and 3 the same...?
-        base_similarity = str_compare(search_str, self.base_node.base_str)
 
-        if base_similarity == 0:
+        # Base node insert
+        if self.base_node.nodes[int(search_str[0])] == None:
             tmp = RT_Node()
             tmp.base_str = search_str
             tmp.result = result
             self.base_node.nodes[int(search_str[0])] = tmp
 
             return
-        else:
+
+        iter_node = self.base_node.nodes[int(search_str[0])]
+
+        while True:
+            base_similarity = str_compare(search_str, iter_node.base_str)
+            crop_search_str = search_str[base_similarity:]
+            search_index = int(crop_search_str[0])
+
+            if iter_node.nodes[search_index] == None:
+                tmp = RT_Node()
+                tmp.base_str = crop_search_str
+                tmp.result = result
+                self.base_node.nodes[int(search_str[0])] = tmp
+
+                return
+            else:
+
+                if len(iter_node.base_str) > base_similarity:
+                    # Substitute root with the new node, with two leaves
+                    # Before ---[P1] --...
+                    # After:
+                    #             --[N2]
+                    # ------[N1]-|--[P1]
+
+                    prev_root_node_index = int(self.base_node.base_str[base_similarity + 1])
+                    new_root_node = RT_Node()
+                    new_root_node.base_str = self.base_node.base_str[0:base_similarity]
+                    new_root_node.nodes[prev_root_node_index] = self.base_node
+                    new_root_node.nodes[prev_root_node_index].base_str = new_root_node.nodes[prev_root_node_index].base_str[base_similarity+1:]
+
+                    self.base_node = new_root_node
