56b75f0d8a22249492ba29c16b2bb21392058506
Juan S. Marquerie <jsmarquerie@gmail.com>
2025-09-20 23:53:19 +0200

    revised

--- TexPackResize.py
+++ TexPackResize.py
@@ -26,16 +26,19 @@ def image_scale(img_adress, result_img_adress, new_size):
         B = img.getchannel('B')
         alpha = np.asarray(img.getchannel('A'))
         alpha = np.clip(alpha, 0.0001, 10000.0)
-        img = Image.merge('RGBA', (R, G, B, Image.fromarray(alpha.astype(np.uint8))))
-    elif not '_s' in img_adress:
+        img = Image.merge('RGBA', (R, G, B, Image.fromarray(alpha.astype(np.uint8))))    
+
+    img = img.resize(new_size, Image.BICUBIC)
+
+    if not '_s.' in img_adress and not '_n.' in img_adress:
         R = img.getchannel('R')
         G = img.getchannel('G')
         B = img.getchannel('B')
         alpha = np.asarray(img.getchannel('A'))
-        alpha = np.where(alpha > 0.5, 1.0, 0.0)
+        alpha = np.where(alpha > 0.5, 255.0, 0.0)
         img = Image.merge('RGBA', (R, G, B, Image.fromarray(alpha.astype(np.uint8))))
 
-    img.resize(new_size, Image.BICUBIC).save(result_img_adress)
+    img.save(result_img_adress)
 
 def img_scale_adapter(x):
     if x[1] == None:
